8cfd6ea4f4351f06607ae80a0f92578b6dd8af07
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2024-06-06 11:58:09 +0300

    Format all Clojure files.

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -2,64 +2,38 @@
   #_{:clj-kondo/ignore [:refer-all]}
   (:require [koan-engine.core :refer :all]))
 
-(defn is-even? [n]
-  (if (= n 0)
-    true
-    (not (is-even? (dec n)))))
+(defn is-even? [n] (if (= n 0) true (not (is-even? (dec n)))))
 
-(defn is-even-bigint? [n]
-  (loop [n   n
-         acc true]
-    (if (= n 0)
-      acc
-      (recur (dec n) (not acc)))))
+(defn is-even-bigint?
+  [n]
+  (loop [n n acc true] (if (= n 0) acc (recur (dec n) (not acc)))))
 
-(defn recursive-reverse [coll]
+(defn recursive-reverse
+  [coll]
   (loop [acc []
          coll coll]
     (let [next-val (peek coll)
           no-more-values? (nil? next-val)]
-      (if no-more-values?
-        acc
-        (recur (conj acc next-val) (pop coll))))))
+      (if no-more-values? acc (recur (conj acc next-val) (pop coll))))))
 
-(defn factorial [n]
-  (loop [n n
-         acc 1]
-    (if (= n 0)
-      acc
-      (recur (- n 1) (* n acc)))))
+(defn factorial
+  [n]
+  (loop [n n acc 1] (if (= n 0) acc (recur (- n 1) (* n acc)))))
 
 (meditations
- "Recursion ends with a base case"
- (= true (is-even? 0))
-
- "And starts by moving toward that base case"
- (= false (is-even? 1))
-
- "Having too many stack frames requires explicit tail calls with recur"
- (= false (is-even-bigint? 100003N))
-
- "Reversing directions is easy when you have not gone far"
- (= '(1) (recursive-reverse [1]))
-
- "Yet it becomes more difficult the more steps you take"
- (= '(6 5 4 3 2) (recursive-reverse [2 3 4 5 6]))
-
- "Simple things may appear simple"
- (= 1 (factorial 1))
-
- "They may require other simple steps"
- (= 2 (factorial 2))
-
- "Sometimes a slightly bigger step is necessary"
- (= 6 (factorial 3))
-
- "And eventually you must think harder"
- (= 24 (factorial 4))
-
- "You can even deal with very large numbers"
- (< 1000000000000000000000000N (factorial 1000N))
-
- "But what happens when the machine limits you?"
- (< 1000000000000000000000000N (factorial 100003N)))
+  "Recursion ends with a base case" (= true (is-even? 0))
+  "And starts by moving toward that base case" (= false (is-even? 1))
+  "Having too many stack frames requires explicit tail calls with recur"
+    (= false (is-even-bigint? 100003N))
+  "Reversing directions is easy when you have not gone far"
+    (= '(1) (recursive-reverse [1]))
+  "Yet it becomes more difficult the more steps you take"
+    (= '(6 5 4 3 2) (recursive-reverse [2 3 4 5 6]))
+  "Simple things may appear simple" (= 1 (factorial 1))
+  "They may require other simple steps" (= 2 (factorial 2))
+  "Sometimes a slightly bigger step is necessary" (= 6 (factorial 3))
+  "And eventually you must think harder" (= 24 (factorial 4))
+  "You can even deal with very large numbers" (< 1000000000000000000000000N
+                                                 (factorial 1000N))
+  "But what happens when the machine limits you?" (< 1000000000000000000000000N
+                                                     (factorial 100003N)))
--- src/koans/20_java_interop.clj
+++ src/koans/20_java_interop.clj
@@ -3,18 +3,22 @@
   (:require [koan-engine.core :refer :all]))
 
 (meditations
- "You may have done more with Java than you know"
- (= java.lang.String (class "warfare")) ; hint: try typing (javadoc "warfare") in the REPL
-
- "The dot signifies easy and direct Java interoperation"
- (= "SELECT * FROM" (.toUpperCase "select * from"))
-
- "But instance method calls are very different from normal functions"
- (= ["SELECT" "FROM" "WHERE"] (map (fn [x] (.toUpperCase x)) ["select" "from" "where"]))
-
- "Constructing might be harder than breaking"
- (= 10 (let [latch (java.util.concurrent.CountDownLatch. 10)]
+  "You may have done more with Java than you know" (= java.lang.String
+                                                      (class "warfare")) ; hint:
+                                                                         ; try
+                                                                         ; typing
+                                                                         ; (javadoc
+                                                                         ; "warfare")
+                                                                         ; in
+                                                                         ; the
+                                                                         ; REPL
+  "The dot signifies easy and direct Java interoperation"
+    (= "SELECT * FROM" (.toUpperCase "select * from"))
+  "But instance method calls are very different from normal functions"
+    (= ["SELECT" "FROM" "WHERE"]
+       (map (fn [x] (.toUpperCase x)) ["select" "from" "where"]))
+  "Constructing might be harder than breaking"
+    (= 10
+       (let [latch (java.util.concurrent.CountDownLatch. 10)]
          (.getCount latch)))
-
- "Static methods are slashing prices!"
- (== 1024.0 (Math/pow 2 10)))
+  "Static methods are slashing prices!" (== 1024.0 (Math/pow 2 10)))
