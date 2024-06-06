2df0cb60e625c6a21d76b72f40abae8e2f6d3eab
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2024-06-06 11:56:00 +0300

    Complete 27_multimethods.

--- src/koans/27_multimethods.clj
+++ src/koans/27_multimethods.clj
@@ -1,22 +1,19 @@
 (ns koans.27-multimethods
+  #_{:clj-kondo/ignore [:refer-all]}
   (:require [koan-engine.core :refer :all]))
 
-(defmulti multimethod-without-args
-  (fn [keyword-arg] keyword-arg))
+(defmulti multimethod-without-args (fn [keyword-arg] keyword-arg))
 
-(defmethod multimethod-without-args :first [_]
-  (str "Hello, World!"))
+(defmethod multimethod-without-args :first [_] (str "Hello, World!"))
 
-(defmethod multimethod-without-args :second [_]
-  (str "Hello there"))
+(defmethod multimethod-without-args :second [_] (str "Hello there"))
 
-(defmulti multimethod-with-args
-  (fn [opt-one opt-two] opt-one))
+(defmulti multimethod-with-args (fn [opt-one opt-two] opt-one))
 
-(defmethod multimethod-with-args :path-one [_ opts]
-  (:first-opt opts))
+(defmethod multimethod-with-args :path-one [_ opts] (:first-opt opts))
 
-(defmethod multimethod-with-args :path-two [_ opts]
+(defmethod multimethod-with-args :path-two
+  [_ opts]
   (let [numbers (:second-opt opts)]
     (->> numbers
          (map inc)
@@ -25,20 +22,11 @@
 (defmethod multimethod-with-args :path-three [_])
 
 (meditations
- "A multimethod takes one or more arguments to dispatch on"
- (= __
-    (multimethod-without-args :first))
-
- "Though it can be ignored and represented by _ in defmethods"
- (= __
-    (multimethod-without-args :second))
-
- "Alternatively, we can use the arguments in defmethods"
- (= __
-    (multimethod-with-args :path-one {:first-opt 1
-                                      :second-opt 2}))
-
- "This allows us to do something different in each method implementation"
- (= __
-    (multimethod-with-args :path-two {:first-opt 1
-                                      :second-opt [0 1 2]})))
+  "A multimethod takes one or more arguments to dispatch on"
+    (= "Hello, World!" (multimethod-without-args :first))
+  "Though it can be ignored and represented by _ in defmethods"
+    (= "Hello there" (multimethod-without-args :second))
+  "Alternatively, we can use the arguments in defmethods"
+    (= 1 (multimethod-with-args :path-one {:first-opt 1, :second-opt 2}))
+  "This allows us to do something different in each method implementation"
+    (= 6 (multimethod-with-args :path-two {:first-opt 1, :second-opt [0 1 2]})))
